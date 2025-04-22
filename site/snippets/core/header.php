6fae25d13d4e2fbbaef859f74758378d31c98edc
Dario Voll <dariovoll@gmail.com>
2025-04-23 00:37:18 +0200

    Fix header height

--- site/snippets/core/header.php
+++ site/snippets/core/header.php
@@ -6,7 +6,7 @@
     <dvll-header class="lg:container mx-auto dvll-container group">
         <div class="col-span-full flex flex-row gap-6 items-center justify-between py-4 md:py-6 px-4 md:px-6">
             <a href="<?= $site->url() ?>" class="flex items-center gap-2" title="<?= $site->title()->escape() ?>">
-                <img src=" /assets/logos/logo.svg" alt="<?= $site->title()->escape() ?>" class="w-[8rem] md:w-[11.5rem] transition-transform origin-left md:origin-top-left group-[.scrolled]:scale-75" />
+                <img src=" /assets/logos/logo.svg" alt="<?= $site->title()->escape() ?>" class="w-[8rem] h-[55.1px] md:w-[11.5rem] md:h-[73.4px] transition-transform origin-left md:origin-top-left group-[.scrolled]:scale-75" />
             </a>
             <div class="nav py-2 pl-4 md:-mr-3">
                 <button class="md:hidden main-nav-button btn btn--secondary">Menü öffnen</button>
