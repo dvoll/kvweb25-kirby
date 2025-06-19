a14035f9d4f8c4cf3bfe14d60dee624a235c54b4
Dario Voll <dariovoll@gmail.com>
2025-06-20 00:11:01 +0200

    Add event refresh kirby 5 panel button

--- plugins/kirby-events/index.php
+++ plugins/kirby-events/index.php
@@ -22,4 +22,41 @@ App::plugin('dvll/kirby-events', [
         'event.ics' => __DIR__ . '/templates/event.ics.php',
         'events' => __DIR__ . '/templates/events.php'
     ],
+    'areas' => [
+        'dvll-events-area' => function (App $kirby) {
+            return [
+                'buttons' => [
+                    'refresh-events' => function () {
+                        return [
+                            'icon'     => 'refresh',
+                            'text'     => 'Termine aktualisieren',
+                            'dialog' => 'dvll-events/refresh'
+                        ];
+                    }
+                ],
+                'dialogs' => [
+                    'dvll-events/refresh' => [
+                        // the load event is creating a GET route at
+                        // `/panel/dialogs/{pattern}`;
+                        // it returns the setup for the dialog, including
+                        // used component, buttons, props, etc.
+                        'load' => function () {
+                            return [
+                                'component' => 'k-text-dialog',
+                                'props' => [
+                                    'text' => 'Mit dem Klicken auf "OK" werden erneut alle Termine aus dem verknüpften Google Kalender abgerufen.',
+                                ]
+                            ];
+                        },
+                        'submit' => function () use ($kirby) {
+                            $cache = $kirby->cache('dvll.kirby-events');
+                            $cache->set('events', null); // Invalidate cache
+                            kirbylog('[dvll.kirby-events] Event cache invalidated through panel. Fetching new events from Google Calendar.');
+                            return true;
+                        }
+                    ]
+                    ]
+            ];
+        }
+    ]
 ]);
