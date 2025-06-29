2ba5b76cd441fa34b4dfce7ef8456331d76e3b20
Dario Voll <dariovoll@gmail.com>
2025-06-29 15:23:35 +0200

    FIx heading rendering for &

--- plugins/sitepackage/blocks/heading/heading.php
+++ plugins/sitepackage/blocks/heading/heading.php
@@ -4,5 +4,5 @@
 ?>
 
 <div class="dvll-block dvll-block--narrow dvll-block--gap-sm">
-    <<?= $level = $block->level()->or('h2') ?> class="heading-lv2"><?= $block->text()->html() ?></<?= $level ?>>
+    <<?= $level = $block->level()->or('h2') ?> class="heading-lv2"><?= $block->text() ?></<?= $level ?>>
 </div>
