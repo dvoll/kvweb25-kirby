8179627aefa9e2c0cf7758944d90f91e39b1cd57
Dario Voll <dariovoll@gmail.com>
2025-07-30 23:23:24 +0200

    Add balanced text to stage headlines

--- plugins/sitepackage/blocks/stage-text-image/stage-text-image.php
+++ plugins/sitepackage/blocks/stage-text-image/stage-text-image.php
@@ -21,7 +21,7 @@ $sizes = [
 <div class="dvll-block dvll-block--wide flex flex-col-reverse gap-y-8 md:grid grid-cols-subgrid">
     <div class="col-start-1 col-end-4 md:grid grid-cols-1 grid-rows-[1fr_auto_2fr]">
         <div class="row-start-2 md:py-4 lg:pr-6">
-            <h1 class="heading-title mb-6"><?= $page->myTitle() ?></h1>
+            <h1 class="heading-title mb-6 text-balance"><?= $page->myTitle() ?></h1>
             <p class="typo">
                 <?= $block->description()->kirbytext() ?>
             </p>
