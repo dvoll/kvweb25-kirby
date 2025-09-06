bd5aa364f6e4012738241b22e73f530874cd46e1
Dario Voll <dariovoll@gmail.com>
2025-09-06 23:08:58 +0200

    Fix csp nonce problems and use js approach for pictures
    This reverts commit bb825be08707e06993650cb38f6e29b47977d71d.

--- site/snippets/picture.php
+++ site/snippets/picture.php
@@ -49,24 +49,11 @@ foreach ($srcSets as $setKey => $set) {
 $defaultSrcset = A::first($srcsetsDefault);
 
 if (is_a($image, 'Kirby\Cms\File') || is_a($image, 'Kirby\Filesystem\Asset')) :
-    $uniqueId = 'picture-style-' . md5($image->id() . $preset . ($cropRatio ?? '') . ($responsive ? 'responsive' : ''));
-    $cropRatioStyle = $cropRatio ?? $image->ratio();
     $focusPosition = $responsive && $image->focus()->isNotEmpty() ? $image->focus() : '50% 50%';
 ?>
 
-    <style nonce="<?= page()->nonce('picture-nonce') ?>">
-        .<?= $uniqueId ?> {
-            --ratio: <?= $cropRatioStyle ?>; /* Currently not used! */
-        }
-        <?php if ($responsive): ?>
-        .<?= $uniqueId ?> > img {
-            object-position: <?= $focusPosition ?>;
-        }
-        <?php endif; ?>
-    </style>
-
     <picture <?= attr([
-                    'class' => [$class ?? '', $uniqueId],
+                    'class' => [$class ?? ''],
                     ...$attr
                 ]) ?>>
 
@@ -88,7 +75,10 @@ if (is_a($image, 'Kirby\Cms\File') || is_a($image, 'Kirby\Filesystem\Asset')) :
                         'height' => $cropRatio ? floor($image->thumb($defaultSrcset)->width() / $cropRatio) : $image->thumb($defaultSrcset)->height(),
                         'alt' => $alt ?? (is_a($image, 'Kirby\Cms\File') ? $image->alt() : null),
                         'loading' => $lazy ? "lazy" : null,
-                        'class' => [$imgClass ?? 'w-full']
+                        'class' => [$imgClass ?? 'w-full'],
+                        ':style' => json_encode([
+                            'object-position' => $focusPosition
+                        ]),
                     ]) ?>>
 
         <?php endif ?>
