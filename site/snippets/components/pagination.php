56f99fca38cd0e7d304220e9460f6e4198d63e77
Dario Voll <dariovoll@gmail.com>
2025-11-30 17:06:46 +0100

    Add blogpost pagination

--- site/snippets/components/pagination.php
+++ site/snippets/components/pagination.php
@@ -1,22 +1,29 @@
+<?php
+
+$labelNext = $labelNext ?? 'Mehr Termine';
+$labelPrev = $labelPrev ?? 'Frühere Termine';
+
+?>
+
 <?php if ($pagination && $pagination->hasPages()): ?>
     <nav class="flex gap-6">
 
         <?php if ($pagination->hasPrevPage()): ?>
             <a class="btn btn--secondary btn--icon-left" href="<?= $pagination->prevPageURL() ?>">
-                <?= snippet('elements/icon', ['icon' => 'arrow-left']) ?>Frühere Termine
+                <?= snippet('elements/icon', ['icon' => 'arrow-left']) ?><?= $labelPrev ?>
             </a>
         <?php else: ?>
             <span class="btn btn--secondary btn--icon-left disabled" aria-hidden>
-                <?= snippet('elements/icon', ['icon' => 'arrow-left']) ?>Frühere Termine
+                <?= snippet('elements/icon', ['icon' => 'arrow-left']) ?><?= $labelPrev ?>
             </span>
         <?php endif ?>
         <?php if ($pagination->hasNextPage()): ?>
             <a class="btn btn--secondary" href="<?= $pagination->nextPageURL() ?>">
-                Mehr Termine<?= snippet('elements/icon', ['icon' => 'arrow-right']) ?>
+                <?= $labelNext ?><?= snippet('elements/icon', ['icon' => 'arrow-right']) ?>
             </a>
         <?php else: ?>
             <span class="btn btn--secondary disabled" aria-hidden>
-                Mehr Termine<?= snippet('elements/icon', ['icon' => 'arrow-right']) ?>
+                <?= $labelNext ?><?= snippet('elements/icon', ['icon' => 'arrow-right']) ?>
             </span>
         <?php endif ?>
 
--- site/templates/blogposts.php
+++ site/templates/blogposts.php
@@ -6,6 +6,9 @@
  * @var Kirby\Cms\Site $site
  */
 
+$paginatedList = $page->children()->listed()->sortBy('date', 'desc')->paginate(9);
+$pagination = $paginatedList->pagination();
+
 snippet('layout', slots: true); ?>
 
 <?php snippet('core/stage'); ?>
@@ -13,7 +16,7 @@ snippet('layout', slots: true); ?>
 <section class="dvll-section">
     <div class="dvll-section__layout">
         <div class="dvll-block dvll-block--wide grid grid-cols-(--dvll-card-grid-cols--small) gap-4 md:gap-6 md:justify-center auto-rows-fr">
-            <?php foreach ($page->children()->listed()->sortBy('date', 'desc') as $post): ?>
+            <?php foreach ($paginatedList as $post): ?>
                 <?= snippet('components/blogpost-card', [
                     'title' => $post->title(),
                     'text' => $post->text()->excerpt(140),
@@ -22,6 +25,15 @@ snippet('layout', slots: true); ?>
                 ]) ?>
             <?php endforeach ?>
         </div>
+        <?php if ($pagination && $pagination->hasPages()): ?>
+            <div class="dvll-block dvll-block--narrow">
+                <?= snippet('components/pagination', [
+                    'pagination' => $pagination,
+                    'labelNext' => 'Ältere Beiträge',
+                    'labelPrev' => 'Neuere Beiträge',
+                ]) ?>
+            </div>
+        <?php endif ?>
     </div>
 </section>
 <?php endsnippet() ?>
